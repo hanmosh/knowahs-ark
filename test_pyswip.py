7c305b6cd208b1ddd357ce6920c2c8e6822eb90c
Swayam Chidrawar <schidraw@calpoly.edu>
2025-05-07 16:14:03 -0700

    Add: requirements

--- test_pyswip.py
+++
@@ -1,6 +0,0 @@
-from pyswip import Prolog
-Prolog.assertz("father(michael,john)")
-Prolog.assertz("father(michael,gina)")
-list(Prolog.query("father(michael,X)")) == [{'X': 'john'}, {'X': 'gina'}]
-for soln in Prolog.query("father(X,Y)"):
-    print(soln["X"], "is the father of", soln["Y"])
